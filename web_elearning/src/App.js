39cb70720fc8998f25002be1be4ef3dc7343ea15
Adam Surya <surya.adam24@gmail.com>
2021-11-15 20:52:45 +0700

    add: route

--- web_elearning/src/App.js
+++ web_elearning/src/App.js
@@ -1,10 +1,12 @@
 import Signup from './Pages/Signup/Signup'
 import Home from './Pages/Home/Home';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 import Event from './Pages/Event/Event';
 // import Account from './Pages/Account/Account';
 import Account from './Pages/Account/Account';
 import Signin from './Pages/Signin/Signin';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const theme = createTheme({
@@ -15,15 +17,27 @@ function App() {
     }
   })
   return (
-    <div className="App">
+    // <Provider store={[]}>
+      <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" exact component={Signin} />
+          <Route path="/home" component={Home} />
+          <Route path="/register" component={Signup} />
+          <Route path="/event" component={Event} />
+          <Route path="/account" component={Account} />
+        </Routes>
+      </Router>
       {/* <Signin /> */}
       {/* <Signup /> */}
-      <Event />
+      {/* <Event /> */}
       {/* <Account /> */}
       {/* <Account /> */}
       {/* <Signin /> */}
       {/* <Home /> */}
     </div>
+    // </Provider>
+    
     // <ThemeProvider theme={theme}>
     //   
     // </ThemeProvider>
